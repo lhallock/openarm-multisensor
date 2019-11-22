3c7df06c204772336b46b49d7219749a2c089498
Laura Hallock <lhallock@eecs.berkeley.edu>
2019-11-22 12:16:51 -0800

    PoC plotting capability

--- run_multisensorimport.py
+++ run_multisensorimport.py
@@ -10,6 +10,10 @@ Todo:
     implement all the things!
 """
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
 from multisensorimport.dataobj import trialdata as td
 
 READ_PATH = '/home/lhallock/Dropbox/DYNAMIC/Research/MM/code/openarm-multisensor/sandbox/data/seg_data_US.mat'
@@ -19,6 +23,16 @@ def main():
     """Execute all EMBC 2020 data analysis."""
     data1 = td.TrialData.from_preprocessed_mat_file(READ_PATH, 'Sub1', 1)
     print(data1.data_emg.data.shape)
+
+    sns.set()
+
+    fig, axs = plt.subplots(3)
+    fig.suptitle('test plot')
+    axs[0].plot(data1.data_emg.data)
+    axs[1].plot(data1.data_amg.data)
+    axs[2].plot(data1.data_force.data)
+
+    plt.show()
 
 
 if __name__ == "__main__":
