a7990e2e73f0616fc49341bf8392e7d4bbde8885
Laura Hallock <lhallock@eecs.berkeley.edu>
2019-12-11 16:11:24 -0800

    general file names supported for ultrasound script

--- run_ultrasoundviz.py
+++ run_ultrasoundviz.py
@@ -11,6 +11,7 @@ Todo:
 """
 
 import time
+import os
 
 import cv2
 import numpy as np
@@ -22,10 +23,6 @@ READ_PATH = '/home/lhallock/Dropbox/DYNAMIC/Research/MM/code/openarm-multisensor
 
 def main():
     """Execute ultrasound image tracking visualization."""
-    # set frame numbers of desired data
-    ind_low = 1495
-    ind_high = 2282
-
     # set Lucas-Kanade optical flow parameters
     lk_params = dict(winSize=(25, 25),
                      maxLevel=8,
@@ -58,39 +55,43 @@ def main():
     # create named window
     cv2.namedWindow('Frame')
 
-    # read initial frame
-    filename = READ_PATH + str(ind_low) + '.pgm'
-    old_frame = cv2.imread(filename, -1)
-
     # track and display specified points through images
-    for i in range(ind_low + 1, ind_high + 1):
+    first_loop = True
 
-        # read in new frame
-        filename = READ_PATH + str(i) + '.pgm'
-        frame = cv2.imread(filename, -1)
+    for filename in sorted(os.listdir(READ_PATH)):
+        if filename.endswith('.pgm'):
+            filepath = READ_PATH + filename
 
-        # calculate new point locations
-        new_points, status, error = cv2.calcOpticalFlowPyrLK(
-            old_frame, frame, npCurrPoints, None, **lk_params)
+            if first_loop:
+                old_frame = cv2.imread(filepath, -1)
+                first_loop = False
 
-        # save old frame for optical flow calculation
-        old_frame = frame.copy()
+            else:
+                # read in new frame
+                frame = cv2.imread(filepath, -1)
 
-        # reset point locations
-        npCurrPoints = new_points
-        for j in range(len(npCurrPoints)):
-            x, y = npCurrPoints[j].ravel()
-            cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+                # calculate new point locations
+                new_points, status, error = cv2.calcOpticalFlowPyrLK(
+                    old_frame, frame, npCurrPoints, None, **lk_params)
 
-        # display to frame
-        cv2.imshow('Frame', frame)
-        key = cv2.waitKey(1)
-        if key == 27:  # stop on escape key
-            break
-        time.sleep(0.05)
+                # save old frame for optical flow calculation
+                old_frame = frame.copy()
 
-        # append new contour area
-        contour_areas.append(cv2.contourArea(npCurrPoints))
+                # reset point locations
+                npCurrPoints = new_points
+                for i in range(len(npCurrPoints)):
+                    x, y = npCurrPoints[i].ravel()
+                    cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+                # display to frame
+                cv2.imshow('Frame', frame)
+                key = cv2.waitKey(1)
+                if key == 27: # stop on escape key
+                    break
+                time.sleep(0.05)
+
+                # append new contour area
+                contour_areas.append(cv2.contourArea(npCurrPoints))
 
     cv2.destroyAllWindows()
 
