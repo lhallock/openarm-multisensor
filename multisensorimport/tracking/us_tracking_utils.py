dde8fcb85f27256ebd8b30d0a7770fd86512f4b9
Laura Hallock <lhallock@eecs.berkeley.edu>
2019-12-12 13:43:16 -0800

    more refactoring

--- multisensorimport/tracking/us_tracking_utils.py
+++ multisensorimport/tracking/us_tracking_utils.py
@@ -8,6 +8,9 @@ dataobj.TimeSeriesData class.
 
 """
 
+import time
+import os
+
 import cv2
 import numpy as np
 
@@ -49,8 +52,74 @@ def extract_contour_pts(filename):
     return np_points
 
 
-def track_pts(filepath, pts, viz=True):
-    pass
+def track_pts(filedir, pts, lk_params, viz=True):
+    """Track specified points through all (ordered) images in directory.
+
+    This function is used to track, record, and visualize points through a full
+    directory of images, recording values of interest at each frame. In
+    particular, this function is used to calculate the cross-sectional area of
+    the brachioradialis muscle at each frame.
+
+    Args:
+        filedir (str): directory in which files are stored, including final '/'
+        pts (numpy.ndarray): list of points to track; assumed to correspond to
+            the first image file in filedir, and to be in counter-clockwise
+            order (TODO: check this)
+        lk_params (dict): parameters for Lucas-Kanade image tracking in OpenCV
+            TODO: set appropriate default values
+        viz (bool): whether to visualize the tracking process
+
+    Returns:
+        list of contour areas of each frame
+    """
+    # keep track of contour areas
+    contour_areas = []
+    contour_areas.append(cv2.contourArea(pts))
+
+    # create OpenCV window
+    cv2.namedWindow('Frame')
+
+    # track and display specified points through images
+    first_loop = True
+    for filename in sorted(os.listdir(filedir)):
+        if filename.endswith('.pgm'):
+            filepath = filedir + filename
+
+            # if it's the first image, we already have the contour area
+            if first_loop:
+                old_frame = cv2.imread(filepath, -1)
+                first_loop = False
+
+            else:
+                # read in new frame
+                frame = cv2.imread(filepath, -1)
+
+                # calculate new point locations
+                new_pts, status, error = cv2.calcOpticalFlowPyrLK(
+                    old_frame, frame, pts, None, **lk_params)
+
+                # save old frame for optical flow calculation
+                old_frame = frame.copy()
+
+                # reset point locations
+                pts = new_pts
+                for i in range(len(pts)):
+                    x, y = pts[i].ravel()
+                    cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+                # display to frame
+                cv2.imshow('Frame', frame)
+                key = cv2.waitKey(1)
+                if key == 27: # stop on escape key
+                    break
+                time.sleep(0.05)
+
+                # append new contour area
+                contour_areas.append(cv2.contourArea(pts))
+
+    cv2.destroyAllWindows()
+
+    return contour_areas
 
 
 def write_us_csv(outfile, vals, val_labels=None):
--- run_ultrasoundviz.py
+++ run_ultrasoundviz.py
@@ -16,7 +16,7 @@ import os
 import cv2
 import numpy as np
 
-#from multisensorimport.dataobj import trialdata as td
+from multisensorimport.tracking import us_tracking_utils as track
 
 READ_PATH = '/home/lhallock/Dropbox/DYNAMIC/Research/MM/code/openarm-multisensor/sandbox/data/ultrasound_t5w1_expanded/'
 
@@ -31,69 +31,10 @@ def main():
 
     # extract initial contour from keyframe
     keyframe_path = READ_PATH + '0.png'
-    img = cv2.imread(keyframe_path, -1)
-    alpha_channel = img[:, :, 3]
-    _, mask = cv2.threshold(alpha_channel, 254, 255, cv2.THRESH_BINARY)  # binarize mask
-    color = img[:, :, :3]
-    new_img = cv2.bitwise_not(cv2.bitwise_not(color, mask=mask))
-    new_img = (255-new_img)
-    gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL,
-                                           cv2.CHAIN_APPROX_SIMPLE)
+    pts = track.extract_contour_pts(keyframe_path)
 
-    # convert contour to array
-    currPoints = []
-    for i in range(len(contours[0])):
-        currPoints.append(np.array(contours[0][i], dtype=np.float32))
-    npCurrPoints = np.array(currPoints)
-
-    # keep track of contour area
-    contour_areas = []
-    contour_areas.append(cv2.contourArea(npCurrPoints))
-
-    # create named window
-    cv2.namedWindow('Frame')
-
-    # track and display specified points through images
-    first_loop = True
-
-    for filename in sorted(os.listdir(READ_PATH)):
-        if filename.endswith('.pgm'):
-            filepath = READ_PATH + filename
-
-            if first_loop:
-                old_frame = cv2.imread(filepath, -1)
-                first_loop = False
-
-            else:
-                # read in new frame
-                frame = cv2.imread(filepath, -1)
-
-                # calculate new point locations
-                new_points, status, error = cv2.calcOpticalFlowPyrLK(
-                    old_frame, frame, npCurrPoints, None, **lk_params)
-
-                # save old frame for optical flow calculation
-                old_frame = frame.copy()
-
-                # reset point locations
-                npCurrPoints = new_points
-                for i in range(len(npCurrPoints)):
-                    x, y = npCurrPoints[i].ravel()
-                    cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
-
-                # display to frame
-                cv2.imshow('Frame', frame)
-                key = cv2.waitKey(1)
-                if key == 27: # stop on escape key
-                    break
-                time.sleep(0.05)
-
-                # append new contour area
-                contour_areas.append(cv2.contourArea(npCurrPoints))
-
-    cv2.destroyAllWindows()
+    # track points
+    contour_areas = track.track_pts(READ_PATH, pts, lk_params)
 
     # write contour areas to csv file
     out_path = READ_PATH + 'csa.csv'
