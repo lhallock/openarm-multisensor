2bbef350691386fbe6cbd557844077d65bf9f8c2
Laura Hallock <lhallock@eecs.berkeley.edu>
2020-06-16 14:49:29 -0700

    cosmetic linting of image processing utils

--- multisensorimport/tracking/image_proc_utils.py
+++ multisensorimport/tracking/image_proc_utils.py
@@ -1,22 +1,30 @@
-"""
-Methods implementing image processing algorithms such as image filters and edge detectors. 
-"""
+#!/usr/bin/env python3
+"""Utility functions for image processing algorithms.
 
+This module contains functions used within image processing algorithms, such as
+filters and edge detectors.
+"""
 import cv2
 import numpy as np
 import scipy
 
-
 def get_filter_from_num(filter_type):
-    """
-    Maps numbers to corresponding image filters. Mapping is: 1 -> median filter, 2->aggressive (fine) bilateral filter, 3 -> less agressive (course) bilateral filter, 4 -> anisotropic diffusion filter, anything else -> no filter
+    """Map numbers to corresponding image filters.
+
+    This function maps integer values to corresponding image filters for use in
+    TODO. The mapping is as follows:
+        1: median filter
+        2: aggressive (fine) bilateral filter
+        3: less aggressive (coarse) bilateral filter
+        4: anisotropic diffusion filter
+        other: no filter
 
     Args:
-        filter_type: integer determining which filter to use
-    Returns:
-        image filter function. This function takes two arguments (img, run_params)
-    """
+        filter_type (int): integer determining which filter to use
 
+    Returns:
+        image filter function that takes two arguments (img, run_params)
+    """
     filter = None
     if filter_type == 1:
         filter = median_filter
@@ -33,17 +41,15 @@ def get_filter_from_num(filter_type):
 
 # image filtering
 def no_filter(img, run_params):
-    """
-    Applies no filter to the image. Convert to grayscale if the image is color.
+    """Applies no filter and converts image to grayscale (if color).
 
     Args:
-        img: image to be potentially grayscaled
-        run_params: instance of ParamValues class, contains values of parameters used in tracking
+        img (TODO type): image to be (potentially) grayscaled
+        run_params (ParamValues): class containing values of parameters used in tracking
 
     Returns:
-        Grayscaled, non-filtered image
+        TODO type grayscale, non-filtered version of input image
     """
-
     # check if image is color or grayscale, return grayscale version
     if len(img.shape) > 2:
         return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -52,14 +58,14 @@ def no_filter(img, run_params):
 
 
 def median_filter(img, run_params):
-    """
-    Applies a median filter to the given image.
+    """Applies a median filter to the given image.
 
     Args:
-        img: image to be filtered
-        run_params: instance of ParamValues class, contains values of parameters used in tracking
+        img (TODO type): image to be filtered
+        run_params (ParamValues): class containing values of parameters used in tracking
 
-    Returns: median filtered version of the img
+    Returns:
+        TODO type median-filtered version of input image
     """
 
     kernelSize = 5
@@ -67,16 +73,15 @@ def median_filter(img, run_params):
 
 
 def fine_bilateral_filter(img, run_params):
-    """
-    Applies an "aggressive" bilateral filter to the given image.
+    """Applies an "aggressive" bilateral filter to the given image.
 
     Args:
-        img: image to be filtered
-        run_params: instance of ParamValues class, contains values of parameters used in tracking
+        img (TODO type): image to be filtered
+        run_params (ParamValues): class containing values of parameters used in tracking
 
-    Returns: bilateral filtered version of the img
+    Returns:
+        TODO type bilaterally-filtered version of input image
     """
-
     # convert to color (what bilateral filter expects)
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
@@ -91,37 +96,40 @@ def fine_bilateral_filter(img, run_params):
 
 
 def course_bilateral_filter(img, run_params):
-    """
-    Applies a "less aggressive" filter to the given image.
+    """Applies a "less aggressive" bilateral filter to the given image.
 
     Args:
-        img: image to be filtered
-        run_params: instance of ParamValues class, contains values of parameters used in tracking
+        img (TODO type): image to be filtered
+        run_params (ParamValues): class containing values of parameters used in tracking
 
-    Returns: bilateral filtered version of the img
+    Returns:
+        TODO type bilaterally-filtered version of input image
     """
-
+    # convert to color (what bilateral filter expects)
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
     # hyperparameters
     diam = run_params.course_diam
     sigmaColor = run_params.course_sigma_color
     sigmaSpace = run_params.course_sigma_space
     bilateralColor = cv2.bilateralFilter(img, diam, sigmaColor, sigmaSpace)
+
+    # convert back to grayscale and return
     return cv2.cvtColor(bilateralColor, cv2.COLOR_RGB2GRAY)
 
-
 def anisotropic_diffuse(img, run_params):
-    """
-    Applies a Perona-Malik anisotropic diffusion filter to the given image.
+    """Applies a Perona-Malik anisotropic diffusion filter to the given image.
 
     Args:
-        img: image to be filtered
-        run_params: instance of ParamValues class, contains values of parameters used in tracking
+        img (TODO type): image to be filtered
+        run_params (ParamValues): class containing values of parameters used in tracking
 
-    Returns: anisotropic diffused version of the img
+    Returns:
+        TODO type anisotropic-diffused version of input image
     """
-
+    # convert to color
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
     # hyperparameters
     alphaVar = 0.1
     KVar = 5
@@ -130,32 +138,33 @@ def anisotropic_diffuse(img, run_params):
                                                       alpha=alphaVar,
                                                       K=KVar,
                                                       niters=nitersVar)
+
+    # convert back to grayscale and return
     return cv2.cvtColor(diffusedColor, cv2.COLOR_RGB2GRAY)
 
 
 def otsu_binarization(gray_image):
-    """
-    Applies otsu binarization to the given image.
+    """Applies Otsu binarization to the given image.
 
     Args:
-        gray_img: grayscale image to be binarized
+        gray_img (TODO type): grayscale image to be binarized
 
-    Returns: Binarized version of the img
+    Returns:
+        TODO type binarized version of input image
     """
-
     ret2, th2 = cv2.threshold(gray_image, 0, 255,
                               cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return th2
 
 
 def canny(gray_image):
-    """
-    Applies Canny Edge Detection to the given image.
+    """Applies Canny edge detection to the given image.
 
     Args:
-        gray_img: grayscale image in which edges should be fine
+        gray_img (TODO type): grayscale image in which edges should be detected
 
-    Returns: Edges present in given image
+    Returns:
+        TODO type edges present in input image
     """
 
     edges = cv2.Canny(gray_image, 180, 200)
