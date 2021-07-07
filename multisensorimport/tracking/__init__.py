391d125ead4731902f0d6811d05af3e4ad52cc18
Laura Hallock <lhallock@eecs.berkeley.edu>
2021-07-07 11:55:33 -0700

    removing old files from BIOROB only

--- multisensorimport/tracking/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Code to track muscle deformation parameters in sequential ultrasound images.
-
-This package contains modules to track, calculate, and export muscle
-deformation measures through sequential ultrasound images, including muscle
-cross-sectional area (CSA), thickness, and aspect ratio, as well as overall
-contour motion.
-"""
-__version__ = '0.0.0'
